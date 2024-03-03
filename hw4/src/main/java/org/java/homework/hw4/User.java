4e6f9574a690a7fa8289d274040beee78ba98d14
Sveta Anikasova <svetlana.anikasova@gmail.com>
2024-03-03 22:12:37 +0300

    поля в классе User теперь приватные

--- hw4/src/main/java/org/java/homework/hw4/User.java
+++ hw4/src/main/java/org/java/homework/hw4/User.java
@@ -1,8 +1,8 @@
 package org.java.homework.hw4;
 
 public class User {
-    Long id;
-    String username;
+    private Long id;
+    private String username;
 
     public User(Long id, String username) {
         this.id = id;
